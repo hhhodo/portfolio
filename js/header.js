33d4b81c9332967d517e5234e747a6f6ce1e53d7
hhhodo <home212121@gmail.com>
2023-12-26 18:10:18 +0900

    js

--- js/header.js
+++ js/header.js
@@ -1,2 +1,13 @@
+const home = document.querySelector('.main_home');
+const homeHeight = home.offsetHeight;
 const headerGnb = document.querySelector('.header');
-const 
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        headerGnb.style.top = '-8rem';
+    } else {
+        headerGnb.style.top = '0';
+    }
+})
+
+/** jQuery 끝나고 고치기 */
