6f1a15810a91df3d2a135a751b9893671feee2c0
지은 <home212121@gmail.com>
2024-01-01 22:02:42 +0900

    mollu

---
+++ js/smoothscroll.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $(window).scroll( function(){
+			
+        $('main_text_box').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},700);
+            }
+            
+        }); 
+        $('main_intro_text').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight() / 3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},700);
+            }
+            
+        }); 
+    });
+});
