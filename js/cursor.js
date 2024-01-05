66a63af8c6756d693b89580d43f625ee07dbc22a
hhhodo <home212121@gmail.com>
2024-01-05 09:34:11 +0900

    dd

--- js/cursor.js
+++ js/cursor.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+/*window.onload = function () {
     const cursor = document.querySelector('.mouse_cursor');
 
     window.addEventListener("scroll", cursor);
@@ -9,3 +9,135 @@ window.onload = function () {
         cursor.style.top = e.pageY - scrollY + "px";
     }
 }
+==================================
+function mouseMove(e) {
+  e.preventDefault();
+  posX = e.clientX - originX;
+  poxY = e.clientY - originY;
+  
+  if (elmnt.offsetLeft + posX >= 0 && elmnt.offsetLeft + elmnt.offsetWidth + posX <= MAX_WIDTH) {
+    x = posX;
+  }
+  
+  if (elmnt.offsetTop + posY >= 0 && elmnt.offsetTop + elmnt.offsetHeight + posY <= MAX_HEIGHT) {
+    y = posY;
+  }
+  
+  elmnt.style.transform = `translate(${x}px, ${y}px)`;
+}
+==
+function mouseMove(e) {
+  e.preventDefault();
+  
+  setTimeout(() => {
+    const x = e.pageX - target.offsetLeft;
+    const y = e.pageY - target.offsetTop;
+    target.scrollLeft = scrollLeft - ((x - originX) * SPEED);
+    target.scrollTop = scrollTop - ((y - originY) * SPEED);
+  }, 0);
+}
+==
+function keepScrolling() {
+  if (!(isEdge && scrolling) {
+    scrolling = false;
+    return;
+  }
+  
+  // X축
+  if (elmnt.offsetLeft + x + scrollX >= 0 && elmnt.offsetLeft + x + scrollX + elmnt.offsetWidth <= MAX_WIDTH) {
+    canvas.scrollLeft += Math.floor(scrollX);
+    x += Math.floor(scrollX);
+    originX -= Math.floor(scrollX);
+  }
+  
+  // Y축
+  if ((elmnt.offsetTop + y + scrollY) >= 0 && (elmnt.offsetTop + y + scrollY + elmnt.offsetHeight) <= MAX_HEIGHT) {
+    canvas.scrollTop += Math.floor(scrollY);
+    y += Math.floor(scrollY);
+    originY -= Math.floor(scrollY);
+  }
+
+  elmnt.style.transform = `translate(${x}px, ${y}px)`;
+
+  window.requestAnimationFrame(keepScrolling);
+}
+===============================================================
+$(function(){
+
+    // CURSOR
+    var cursor = $(".cursor"),
+    follower = $(".cursor-follower"),
+    cursor_img = $(".cursor_img");
+
+    var posX = 0,
+        posY = 0;
+    var mouseX = 0,
+        mouseY = 0;
+
+    TweenMax.to({}, 0.016, { // 값을 올릴수록 cursor-follower 영역이 늦게 따라옴
+        repeat: -1,
+        onRepeat: function() {
+            posX += (mouseX - posX) / 9;
+            posY += (mouseY - posY) / 9;
+
+            TweenMax.set(follower, {
+                css: {
+                left: posX - 12,
+                top: posY - 12
+                }
+            });
+
+            TweenMax.set(cursor, {
+                css: {
+                left: mouseX,
+                top: mouseY
+                }
+            });
+
+            TweenMax.set(cursor_img, {
+                css: {
+                left: mouseX - 24,
+                top: mouseY - 24
+                }
+            });
+        }
+    });
+
+    // cursor active area
+    $(".active_box").on("mousemove", function(e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }).on("mouseenter", function(e) {
+        $(".cursor, .cursor-follower").css('opacity', 1);
+        $(".cursor_img").css('opacity', 0);
+    }).on("mouseleave", function(e) {
+        $(".cursor, .cursor-follower, .cursor_img").css('opacity', 0);
+    });
+
+    // cursor active area (style_2)
+    $(".cursor_style_2").on("mousemove", function(e) {
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }).on("mouseenter", function(e) {
+        $(".cursor_img").css('opacity', 1);
+        $(".cursor, .cursor-follower").css('opacity', 0);
+    }).on("mouseleave", function(e) {
+        $(".cursor_img").css('opacity', 0);
+        $(".cursor, .cursor-follower").css('opacity', 1);
+    });
+
+    // link area css 
+    $(".link").on("mouseenter", function() {
+        cursor.addClass("active");
+        follower.addClass("active");
+    }).on("mouseleave", function() {
+        cursor.removeClass("active");
+        follower.removeClass("active");
+    })
+
+});
+
+
+
+
+;*/
--- js/header.js
+++ js/header.js
@@ -1,13 +1,63 @@
-const home = document.querySelector('.main_home');
-const homeHeight = home.offsetHeight;
-const headerGnb = document.querySelector('.header');
+const hamBurger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".overlay");
+const mouseCursor = document.querySelector(".mouse_cursor");
+//===============버튼 불러오기====================//
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY > homeHeight / 2) {
-        headerGnb.style.top = '-8rem';
+//==============observer 타겟====================//
+const mainHome = document.querySelector('.main_home');
+const mainIntro = document.querySelector('.main_intro');
+const fooTer = document.querySelector('.footer');
+//첫 페이지 observe
+const targetPage = document.querySelector('.main_home');
+
+//
+window.onload = function() {
+
+const observer = new IntersectionObserver((e) => {
+    
+    //특정 요소 오퍼시티 조절
+    e.forEach((fade) => {
+        if(fade.isIntersecting){
+            fade.target.style.opacity = 1;
+        } else {
+            fade.target.style.opacity = 0;
+        }
+    })
+}, { threshold: 0.3 });
+
+//observe
+observer.observe(mainHome);
+observer.observe(mainIntro);
+observer.observe(fooTer);
+observer.observe(targetPage);
+
+//마우스 커서 변경
+window.addEventListener("mousemove", (e) => {
+    
+    var pY = e.pageY;
+    console.log(pY);
+    if(pY <= 1200){
+        mouseCursor.classList.add('blue_gradient');
     } else {
-        headerGnb.style.top = '0';
-    }
+        mouseCursor.classList.remove('blue_gradient');
+    };
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    mouseCursor.style.left = mouseX + 'px';
+    mouseCursor.style.top = mouseY + 'px';
 })
 
-/** jQuery 끝나고 고치기 */
+//마우스가 첫화면일때
+    
+
+//hamburger 버튼 작동 시
+hamBurger.addEventListener('click', () => {
+
+    hamBurger.classList.toggle('cancel');
+    navMenu.classList.toggle('open');
+    mouseCursor.classList.toggle('cursor_active');
+});
+
+//창을 클릭하면 네비게이션 메뉴 사라지게
+}
